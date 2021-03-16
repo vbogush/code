588309b7a91ae80870e0957ef66e76d91d09839f
vbogush <vyacheslav.bogush@gmail.com>
2021-03-16 07:47:00 +0200

    updated

--- pizza/pizza.py
+++ pizza/pizza.py
@@ -3,57 +3,54 @@ tk=Tk()
 tk.geometry ("440x300")
 tk.title("Pizza")
 
-name=Label(text="Naming",font=("Arial",12,"bold"))
+name=Label(text="Найменування",font=("Arial",12,"bold"))
 name.place(x=20,y=20)
 
 
-price=Label(text="Price, UAH.",font=("Arial",12,"bold"))
+price=Label(text="Ціна, грн..",font=("Arial",12,"bold"))
 price.place(x=150,y=20)
 
 
-number=Label(text="Quantaty",font=("Arial",12,"bold"))
+number=Label(text="Кількість",font=("Arial",12,"bold"))
 number.place(x=230,y=20)
 
 
-cost=Label(text="Total, UAH",font=("Arial",12,"bold"))
+cost=Label(text="Разом, грн.",font=("Arial",12,"bold"))
 cost.place(x=310,y=20)
 
 
-lbl1=Label(text="Pizza",font=("Arial",12))
+lbl1=Label(text="Піца",font=("Arial",12))
 lbl1.place(x=20,y=60)
 
 
-lbl2=Label(text="Icecream",font=("Arial",12))
+lbl2=Label(text="Морозиво",font=("Arial",12))
 lbl2.place(x=20,y=100)
 
 
-lbl3=Label(text="Tasty",font=("Arial",12))
+lbl3=Label(text="Тістечко",font=("Arial",12))
 lbl3.place(x=20,y=140)
 
 
-lbl4=Label(text="Juce",font=("Arial",12))
+lbl4=Label(text="Сік",font=("Arial",12))
 lbl4.place(x=20,y=180)
 
-lbl1=Label(text="Pizza",font=("Arial",12))
-lbl1.place(x=20,y=60)
 
-
-p1=Entry(font="Arial 12",bg="sky blue",justify="center")
+p1=Entry(font="Arial 12",bg="grey",justify="center")
 p1.insert(END,"75")
 p1.place(x=150,y=60,width=60,height=30)
 
 
-p2=Entry(font="Arial 12",bg="sky blue",justify="center")
+p2=Entry(font="Arial 12",bg="grey",justify="center")
 p2.insert(END,"12")
 p2.place(x=150,y=100,width=60,height=30)
 
 
-p3=Entry(font="Arial 12",bg="sky blue",justify="center")
+p3=Entry(font="Arial 12",bg="grey",justify="center")
 p3.insert(END,"16")
 p3.place(x=150,y=140,width=60,height=30)
 
 
-p4=Entry(font="Arial 12",bg="sky blue",justify="center")
+p4=Entry(font="Arial 12",bg="grey",justify="center")
 p4.insert(END,"8")
 p4.place(x=150,y=180,width=60,height=30)
 
@@ -121,32 +118,32 @@ s4.place(x=230,y=170)
 
 
 var1=StringVar()
-c1=Label(text=0,font="Arial 12",bg="deep sky blue",textvariable=var1)
+c1=Label(text=0,font="Arial 12",bg="grey",textvariable=var1)
 c1.place(x=310,y=60,width=60,height=30)
 
 var2=StringVar()
-c2=Label(text=0,font="Arial 12",bg="deep sky blue",textvariable=var2)
+c2=Label(text=0,font="Arial 12",bg="grey",textvariable=var2)
 c2.place(x=310,y=100,width=60,height=30)
 
 var3=StringVar()
-c3=Label(text=0,font="Arial 12",bg="deep sky blue",textvariable=var3)
+c3=Label(text=0,font="Arial 12",bg="grey",textvariable=var3)
 c3.place(x=310,y=140,width=60,height=30)
 
 var4=StringVar()
-c4=Label(text=0,font="Arial 12",bg="deep sky blue",textvariable=var4)
+c4=Label(text=0,font="Arial 12",bg="grey",textvariable=var4)
 c4.place(x=310,y=180,width=60,height=30)
 
 
 
-lbl5=Label(text="Total",font=("Arial",12))
+lbl5=Label(text="Разом",font=("Arial",12))
 lbl5.place(x=20,y=240)
 
 var5=StringVar()
-c5=Label(text=0,font="Arial 12",bg="deep sky blue",textvariable=var5)
+c5=Label(text=0,font="Arial 12",bg="grey",textvariable=var5)
 c5.place(x=200,y=240,width=60,height=30)
 
 
-lbl6=Label(text="UAH",font=("Arial",12))
+lbl6=Label(text="грн",font=("Arial",12))
 lbl6.place(x=270,y=240)
 
 
@@ -156,7 +153,7 @@ def btn_click():
     var5.set(rez(y))
 
 
-btn=Button(text="Calculate",font=("Arial",12),command=btn_click)
+btn=Button(text="Розрахувати",font=("Arial",12),command=btn_click)
 btn.place(x=310,y=240,height=30)
 
 tk.mainloop()
